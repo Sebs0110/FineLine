8a300930b25ae68da2620e7b3cd4d4c060dbc6d2
aysimn <yasmin.d@unochapeco.edu.br>
2026-04-22 20:21:22 -0300

    adicionei a rota em web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\OnibusController;
 
 Route::get('/', function () {
     return view('estrutura');
 });
+
+Route::resource('onibus', OnibusController::class);
